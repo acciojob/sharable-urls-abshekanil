da4611ce3c68af07c8d619d0847af8a421255815
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-10 10:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // your code here
+document.addEventListener('DOMContentLoaded', function(){
+
+	let urlLink = document.getElementById('url');
+	let nameInput = document.getElementById('name');
+	let yearInput = document.getElementById('year');
+	let button = document.getElementById('button');
+
+	button.addEventListener('click', function(){
+		if(nameInput.value !== '' && yearInput.value !== '')
+		{
+			urlLink.innerHTML = 'https://localhost:8080/?name='+nameInput.value+'&year='+yearInput.value;
+		}
+		else if(nameInput.value !== '' && yearInput.value === '' )
+		{
+			urlLink.innerHTML = 'https://localhost:8080/?name='+nameInput.value;
+		}
+		else if(nameInput.value === '' && yearInput.value !== '' )
+		{
+			urlLink.innerHTML = 'https://localhost:8080/?year='+yearInput.value;
+		}
+	});
+	
+});
+
+
+
